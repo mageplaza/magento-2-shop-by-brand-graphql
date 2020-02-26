2c23967f9f79165cb164a8b0d928659fc0b598b6
Vu Van Tu <tuvv@mageplaza.com>
2020-02-26 07:41:49 +0000

    fix brand image url

---
+++ Model/Resolver/Brands/BrandImage.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Mageplaza
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Mageplaza.com license that is
+ * available through the world-wide-web at this URL:
+ * https://www.mageplaza.com/LICENSE.txt
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this extension to newer
+ * version in the future.
+ *
+ * @category    Mageplaza
+ * @package     Mageplaza_ShopbybrandGraphQl
+ * @copyright   Copyright (c) Mageplaza (https://www.mageplaza.com/)
+ * @license     https://www.mageplaza.com/LICENSE.txt
+ */
+declare(strict_types=1);
+
+namespace Mageplaza\ShopbybrandGraphQl\Model\Resolver\Brands;
+
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\GraphQl\Config\Element\Field;
+use Magento\Framework\GraphQl\Query\ResolverInterface;
+use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
+use Mageplaza\Shopbybrand\Helper\Data;
+use Mageplaza\Shopbybrand\Model\Brand;
+
+/**
+ * Class BrandImage
+ * @package Mageplaza\ShopbybrandGraphQl\Model\Resolver\Brands
+ */
+class BrandImage implements ResolverInterface
+{
+    /**
+     * @var Data
+     */
+    protected $helperData;
+
+    /**
+     * BrandImage constructor.
+     *
+     * @param Data $helperData
+     */
+    public function __construct(Data $helperData)
+    {
+        $this->helperData = $helperData;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function resolve(Field $field, $context, ResolveInfo $info, array $value = null, array $args = null)
+    {
+        if (!isset($value['image'])) {
+            throw new LocalizedException(__('"image" value should be specified'));
+        }
+
+        if (!isset($value['model'])) {
+            throw new LocalizedException(__('"model" value should be specified'));
+        }
+
+        /** @var Brand $brand */
+        $brand = $value['model'];
+
+        return $this->helperData->getBrandImageUrl($brand);
+    }
+}
